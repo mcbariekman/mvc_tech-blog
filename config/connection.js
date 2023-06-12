c187feef7bce90c492acc3fcc4829221adcd4194
MC <119978958+mcbariekman@users.noreply.github.com>
2023-06-12 13:27:11 -0700

    server

--- config/connection.js
+++ config/connection.js
@@ -1,52 +1,14 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    }
-  );
-}
+    });
 
-const User = sequelize.define('User', {
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
-
-const Comment = sequelize.define('Comment', {
-  content: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  createdAt: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.NOW
-  }
-});
-
-User.hasMany(Comment, { foreignKey: 'userId' });
-Comment.belongsTo(User, { foreignKey: 'userId' });
-
-module.exports = {
-  sequelize,
-  User,
-  Comment
-};
+module.exports = sequelize;
